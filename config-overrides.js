d3ff280f70557b1fc55bbc37862c96fe0450eed7
hua397 <hua397@gmail.com>
2020-06-17 16:10:15 +0800

    1.修正babel-jest报错 2.修正css相对路径404的问题

--- config-overrides.js
+++ config-overrides.js
@@ -2,15 +2,18 @@ const {
 	override,
 	fixBabelImports,
 	addLessLoader,
+	addWebpackPlugin,
 	addWebpackAlias
 } = require('customize-cra');
+
+const CssUrlRelativePlugin = require('css-url-relative-plugin')
 
 const path = require('path');
 
 // 关闭sourceMap
 process.env.GENERATE_SOURCEMAP = 'false';
 
-module.exports = override(
+module.exports = override(	
 	fixBabelImports('import', {
 		libraryName: 'antd',
 		libraryDirectory: 'es',
@@ -25,5 +28,7 @@ module.exports = override(
 	addLessLoader({
 		javascriptEnabled: true,
 		modifyVars: { '@primary-color': '#1DA57A' }
-	})
+	}),
+
+	addWebpackPlugin(new CssUrlRelativePlugin(/* options */))
 );
--- src/components/ContentV/index.js
+++ src/components/ContentV/index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import './index.less';
 
 class ContentV extends Component {
 	render() {
-		return <div>ContentV</div>;
+		return <div>
+			<p>ContentV</p>
+			<img className="content-title"></img>
+		</div>;
 	}
 }
 
